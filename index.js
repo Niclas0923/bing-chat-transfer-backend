5a3459e4dccdb8fed827414d08964ed79d519d29
Niclas0923 <736012843@qq.com>
2023-05-14 10:38:03 +0800

    更新md文件并添加简单验证。

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ import server from "./mod/server.js";
 const data = JSON.parse(String(fs.readFileSync("./config.json")))
 
 // 开启监听
-server(data)
+if(data.cookie.length === 0) console.error("config.json文件中的cookie值没有设置，请根据md文件中的描述进行设置")
+else server(data)
